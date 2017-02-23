4e8ce23674870f4b9fb67f1f82eb203c8922b65b
Jacob Prinz <jacobeliasprinz@gmail.com>
2017-02-23 15:00:08 -0500

    Initial commit

---
+++ elpa/slime-volleyball-20140717.2141/slime-volleyball-autoloads.el
@@ -0,0 +1,27 @@
+;;; slime-volleyball-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "slime-volleyball" "slime-volleyball.el" (22701
+;;;;;;  60289 880959 391000))
+;;; Generated autoloads from slime-volleyball.el
+
+(autoload 'slime-volleyball "slime-volleyball" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("green-slime.el.gz" "grey-slime.el.gz"
+;;;;;;  "slime-volleyball-pkg.el") (22701 60289 962148 455000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; slime-volleyball-autoloads.el ends here
